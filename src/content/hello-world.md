f2b51f51dd85b3ccafa534a0f21b0b33e5dc9c7f
harmy <hack4cn@gmail.com>
2012-08-17 12:55:03 +0800

    first commit

---
+++ src/content/hello-world.md
@@ -0,0 +1,9 @@
+Title: Hello World
+Date: 2012-08-17
+Intro: 好久沒寫東西了。
+tags: python
+
+趁Namecheap打折註冊了個harmy.me的域名，從今以後開始培養記錄的習慣。
+所以，Hello World!
+
+
---
+++ src/settings.py
@@ -0,0 +1,24 @@
+import os
+
+AUTHOR = 'harmy'
+SITENAME = 'harmy.me'
+
+SITEURL = 'http://harmy.me'
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/category.%s.rss.xml'
+TAG_FEED_RSS = 'feeds/tag.%s.rss.xml'
+
+LOCALE = 'en_US.UTF-8'
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_PAGINATION = 2
+PDF_GENERATOR = False
+RELATIVE_URL = False
+REVERSE_CATEGORY_ORDER = True
+
+THEME = 'theme'
+OUTPUT_PATH = '../'
+STATIC_PATHS = ('files', 'images')
+
+GITHUB_URL = 'http://github.com/harmy/'
+GOOGLE_ANALYTICS = 'UA-34158001-1'
